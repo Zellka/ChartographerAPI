3ebf5fa56c96fe2bc91e70a81c4f57974afc1c53
Zellka <ilona.zelka@gmail.com>
2022-03-09 20:31:26 +0300

    Create ImageNotOverlapException

---
+++ src/main/java/com/example/chartographer/exception/ImageNotOverlapException.java
@@ -0,0 +1,8 @@
+package com.example.chartographer.exception;
+
+public class ImageNotOverlapException extends Exception {
+    public ImageNotOverlapException(String message) {
+        super(message);
+    }
+}
+
